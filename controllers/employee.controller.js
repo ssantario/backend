2e82822cc72babe52a8cd5f110811a1419293384
riviyasha <riviyasha@gmail.com>
2024-07-31 15:55:02 +0700

    lupa commit

--- controllers/employee.controller.js
+++ controllers/employee.controller.js
@@ -24,4 +24,16 @@ exports.getEmployee = async function getEmployee(req, res) {
 
 exports.getOneEmployee = async function getOneEmployee(req, res) {
   // Insert kode GET di sini
+  try {
+    const { id } = req.params;
+    const response = await pg.query('SELECT * FROM employee WHERE id = $1', [id]);
+
+    if (response.rows.length === 0) {
+      return res.status(404).json({ message: 'Employee not found' });
+    }
+
+    res.status(200).json(response.rows[0]);
+  } catch (error) {
+    res.status(500).json(error);
+  }
 };
