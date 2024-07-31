64d94e3ead29f498e09739f010c7b4eae261ba34
riviyasha <riviyasha@gmail.com>
2024-07-31 18:34:12 +0700

    get employee function added

--- controllers/manager.controller.js
+++ controllers/manager.controller.js
@@ -1,6 +1,6 @@
 const pg = require("../utils/connect");
 
-exports.register = async function addEmployee(req, res) {
+exports.register = async function register(req, res) {
   // Insert kode REGISTER di sini
   try {
     const { name, password } = req.body;
@@ -12,14 +12,29 @@ exports.register = async function addEmployee(req, res) {
   }
 };
 
-exports.login = async function addEmployee(req, res) {
+exports.login = async function login(req, res) {
   // Insert kode LOGIN di sini
   try {
     const { name, password } = req.body;
     const response = await pg.query('SELECT * FROM manager WHERE name = $1 AND password = $2', [name, password]);
 
+    if (response.rows.length === 0) throw new error ("Failed to Login");
     res.status(200).json(response.rows);
   } catch (error) {
     res.status(500).json(error);
   }
 };
+
+exports.getManager = async function getManager(req,res){
+  try {
+    const { name } = req.body;
+    const response = await pg.query('SELECT * FROM manager WHERE name = $1', [name]);
+
+    if (response.rows.length === 0) throw new error ("Data not found");
+    const managerInfo = response.rows[0];
+    res.status(200).json({ name: managerInfo.name });
+    }catch (error) {
+      res.status(500).json({ error: error.message });
+    }
+};
+
--- routes/manager.route.js
+++ routes/manager.route.js
@@ -4,6 +4,6 @@ const router = express.Router();
 
 router.post("/login", managerController.login);
 router.post("/register", managerController.register);
-
+router.get("/:name", managerController.getManager);
 
 module.exports = router;
