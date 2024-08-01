2e4d9caa9b35a99c6a0e107ef1a9d38c14e10f01
Muhammad Ikhsan Kurniawan <ragendoft05@gmail.com>
2024-08-01 09:33:46 +0700

    delete employee api

--- routes/employee.route.js
+++ routes/employee.route.js
@@ -3,6 +3,7 @@ const employeeController = require("../controllers/employee.controller");
 const router = express.Router();
 
 router.post("/add", employeeController.addEmployee);
+router.delete("/:id", employeeController.deleteEmployee);
 router.get("/", employeeController.getEmployee);
 router.get("/:id", employeeController.getOneEmployee);
 
